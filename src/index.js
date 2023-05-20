761eb4df946b1ef4e9a1b77410f0116ca97c65f2
zpc <zip_city@163.com>
2023-05-20 15:32:12 +0800

    Lights off

--- src/index.js
+++ src/index.js
@@ -2,84 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+let BlockBlue = '#497bb1';
+let BlockBlack = '#131313';
 let BOARD_W = 10;
 let BOARD_H = 10;
-let WIN_LEN = 5;
 
 function Square(props) {
     return (
-      <button className="square" onClick={ props.onClick }> 
-        {props.value}
+      <button className="square" style={{background: props.background}} onClick={props.onClick}> 
       </button>
     );
 }
 
-function calculateWinner(squares) {
-  function isWinner(i, j, shift) {
-    let step = 1;
-    const [x, y] = shift;
-    const boundary_i = i + x*(WIN_LEN-1), boundary_j = j + y*(WIN_LEN-1);
-    if(boundary_i < 0 || boundary_i >= BOARD_H || boundary_j < 0 || boundary_j >= BOARD_W)
-      return false; 
-    for(step; step < WIN_LEN; step++) {
-      if(squares[i + x*step][j + y*step] != squares[i][j]) {
-        break;
-      }
-    }
-    return step === WIN_LEN;
-  }
-
-  for(let i = 0; i < squares.length; i++) {
-    for(let j = 0; j < squares[0].length; j++) {
-      if(squares[i][j]) {
-        if(isWinner(i, j, [0, 1]) || 
-            isWinner(i, j, [1, 0]) || 
-            isWinner(i, j, [1, 1]) || 
-            isWinner(i, j, [1, -1]))
-          return squares[i][j];
-      }
-    }
-  }
-  return null;
+function getRandomColor() {
+  const color = [BlockBlack, BlockBlue];
+  const randomNum = Math.random();
+  const index = (randomNum < 0.5) ? 0 : 1;
+  return color[index];
 }
+
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array.from({length:BOARD_H}, () => Array(BOARD_W).fill(null)),
-      xIsNext: true,
+      squares: Array.from({length:BOARD_H}, () => Array(BOARD_W).fill(null).map(() => getRandomColor())),
     };
   }
 
   handleClick(i, j) {
-    if(!calculateWinner(this.state.squares) && !this.state.squares[i][j]) {
-      const squares = this.state.squares.slice();
-      squares[i][j] = 'X';
-      if(!this.state.xIsNext) {
-        squares[i][j] = 'O';
+    const squares = this.state.squares.map((x) => x.slice());
+    
+    const neis = [[0, 0], [-1, 0], [1, 0], [0, -1], [0, 1]];
+    for(let index = 0; index < neis.length; index++) {
+      const [di, dj] = neis[index];
+      const x = i + di, y = j + dj;
+      if( 0 <= x && x < squares[0].length &&  0 <= y && y < squares.length) {
+        squares[x][y] = (squares[x][y] === BlockBlack) ? BlockBlue : BlockBlack;
       }
-      this.setState({
-        squares: squares, 
-        xIsNext: !this.state.xIsNext
-      });
     }
+    this.setState({
+      squares: squares,
+    })
   }
 
   renderSquare(i, j) {
-    return <Square value={this.state.squares[i][j]} onClick={() => this.handleClick(i, j) } />;
+    return <Square background={this.state.squares[i][j]} onClick={() => this.handleClick(i, j) } />;
   }
 
   render() {
-    let status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    const winner = calculateWinner(this.state.squares)
-    if(winner) {
-      status = 'Winner is ' + winner;
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         {Array(BOARD_H).fill(null).map((_, row) => (
           <div key={row} className="board-row">
             {Array(BOARD_W).fill(null).map((_, col) => this.renderSquare(row, col))}
