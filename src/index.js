71409218c39c1e7a9584a4b1747d9c0f1849bdba
zpc <zip_city@163.com>
2023-05-20 15:25:55 +0800

    Upload template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,112 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let BOARD_W = 10;
+let BOARD_H = 10;
+let WIN_LEN = 5;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Square(props) {
+    return (
+      <button className="square" onClick={ props.onClick }> 
+        {props.value}
+      </button>
+    );
+}
+
+function calculateWinner(squares) {
+  function isWinner(i, j, shift) {
+    let step = 1;
+    const [x, y] = shift;
+    const boundary_i = i + x*(WIN_LEN-1), boundary_j = j + y*(WIN_LEN-1);
+    if(boundary_i < 0 || boundary_i >= BOARD_H || boundary_j < 0 || boundary_j >= BOARD_W)
+      return false; 
+    for(step; step < WIN_LEN; step++) {
+      if(squares[i + x*step][j + y*step] != squares[i][j]) {
+        break;
+      }
+    }
+    return step === WIN_LEN;
+  }
+
+  for(let i = 0; i < squares.length; i++) {
+    for(let j = 0; j < squares[0].length; j++) {
+      if(squares[i][j]) {
+        if(isWinner(i, j, [0, 1]) || 
+            isWinner(i, j, [1, 0]) || 
+            isWinner(i, j, [1, 1]) || 
+            isWinner(i, j, [1, -1]))
+          return squares[i][j];
+      }
+    }
+  }
+  return null;
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array.from({length:BOARD_H}, () => Array(BOARD_W).fill(null)),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i, j) {
+    if(!calculateWinner(this.state.squares) && !this.state.squares[i][j]) {
+      const squares = this.state.squares.slice();
+      squares[i][j] = 'X';
+      if(!this.state.xIsNext) {
+        squares[i][j] = 'O';
+      }
+      this.setState({
+        squares: squares, 
+        xIsNext: !this.state.xIsNext
+      });
+    }
+  }
+
+  renderSquare(i, j) {
+    return <Square value={this.state.squares[i][j]} onClick={() => this.handleClick(i, j) } />;
+  }
+
+  render() {
+    let status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares)
+    if(winner) {
+      status = 'Winner is ' + winner;
+    }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        {Array(BOARD_H).fill(null).map((_, row) => (
+          <div key={row} className="board-row">
+            {Array(BOARD_W).fill(null).map((_, col) => this.renderSquare(row, col))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
